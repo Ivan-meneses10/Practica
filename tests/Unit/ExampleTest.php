841a0b1016f3f2f6862c75eb9ea85dcf9c35ad66
Ivan-meneses10 <ivanmeneses222@gmail.com>
2023-11-23 17:32:44 -0600

    Prueba fallo

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,4 +15,34 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testSuma()
+    {  
+        $resultado = (new \App\Http\Controllers\Controller)->suma(3, 5);
+        $this->assertEquals(7, $resultado);
+    }
+
+    public function testMultiplicion()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->multiplicion(4, 6);
+        $this->assertEquals(20, $resultado);
+    }
+
+    public function testEsPositivo()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->esPositivo(7);
+        $this->assertTrue($resultado);
+    }
+
+    public function testEsNumeroPar()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->esNumeroPar(10);
+        $this->assertTrue($resultado);
+    }
+
+    public function testCalcularAreaTriangulo()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->calcularAreaTriangulo(4, 8);
+        $this->assertEquals(16, $resultado);
+    }
 }
